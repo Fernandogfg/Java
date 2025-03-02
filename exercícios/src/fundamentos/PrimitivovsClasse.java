d1dc9f51ce358523517a82659ecc2b9ddc0cf360
Fernando <fernandofgfg10@gmail.com>
2025-03-02 10:41:23 -0400

    Primitivos das classes

---
+++ exercícios/src/fundamentos/PrimitivovsClasse.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class PrimitivovsClasse {
+	public static void main(String[] args) {
+		String s ="Texto";
+		s.toUpperCase();
+		//Wrappers são a versão objeto dos tipos primitivos
+		int a = 123;
+		System.out.println(a);
+	}
+}
