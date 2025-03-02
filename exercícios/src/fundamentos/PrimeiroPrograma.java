65b8f21b4c6dc2b662ca563bd622623bd0375bec
Fernando <fernandofgfg10@gmail.com>
2025-03-02 10:40:47 -0400

    primeiro programa em java

---
+++ exercícios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package fundamentos;
+/**
+ * Essa classe representa os fundamentos.
+ * 
+ * @author fernando gabriel
+ * @since 1.0
+ */
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//Uma sentença de código termina com ";"
+		//Mais um comentário...
+		System.out.println("Primeiro programa Parte 1!!!");
+		
+		/*
+		 Linha 1
+		 linha 2
+		 linha 3...
+		 */
+		System.out.println("Primeiro programa Parte 2!!!");
+		System.out.println("Fim");//aqui também funciona
+
+	}
+}
