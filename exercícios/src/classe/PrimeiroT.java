38a8f7cec4c07452c09440e06e6db678966c2a4e
Fernando <fernandofgfg10@gmail.com>
2025-03-25 18:52:40 -0400

    primeiro Teste

---
+++ exercícios/src/classe/PrimeiroT.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class PrimeiroT {
+	int a = 3;
+static int p = 4;
+	public static void main(String[] args) {
+		PrimeiroT b = new PrimeiroT();
+		System.out.println(b.a);
+		
+		System.out.println(p);
+	}
+}
