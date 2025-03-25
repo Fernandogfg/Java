b1200d054180b6b01e62a2c48da4812803bdc172
Fernando <fernandofgfg10@gmail.com>
2025-03-25 18:53:18 -0400

    desafio de conclusão do modulo

---
+++ exercícios/src/classe/DesafioModulo.java
@@ -0,0 +1,7 @@
+package classe;
+
+public class DesafioModulo {
+	//crie 3 classes chamadas Pessoa, comida e jantar.
+	//classe jantar vai instanciar uma pessoa e duas comidas e fazer com que a pessoa coma
+	// a classe pessoa vai conter o nome(string) e o peso(double) e metodo chamado comer vai receber como paramentro um obj da classe comida
+}
